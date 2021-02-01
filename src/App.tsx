178205fc53d92257a5c88f44aa947b4cca7d26b6
glecun <gregoire.lecun@vidal.fr>
2021-02-01 15:26:15 +0100

    Add a stop all sounds button

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import './App.global.css';
 import { FaCog, FaRegKeyboard } from 'react-icons/fa/';
+import { useState } from 'react';
 import SoundsView from './application/view/SoundsView';
 import SettingsView from './application/view/SettingsView';
 
 export default function App() {
+  const [stopAllSounds, setStopAllSounds] = useState([] as (() => void)[]);
+  const registerSound = (stopSound: () => void) =>
+    setStopAllSounds(stopAllSounds.concat(stopSound));
+
   return (
     <Router>
       <div className="app-view">
@@ -23,7 +28,12 @@ export default function App() {
           <ToastContainer />
           <Switch>
             <Route path="/settings" component={SettingsView} />
-            <Route path="/" component={SoundsView} />
+            <Route path="/">
+              <SoundsView
+                stopAllSounds={stopAllSounds}
+                registerSound={registerSound}
+              />
+            </Route>
           </Switch>
         </div>
       </div>
--- src/application/view/SoundsView.tsx
+++ src/application/view/SoundsView.tsx
@@ -1,24 +1,42 @@
 import React, { useEffect, useState } from 'react';
+import { FaStop } from 'react-icons/fa';
 import Sound from '../../domain/entities/Sound';
 import { getSounds } from '../../domain/SoudboardDomain';
 import SoundComponent from '../component/SoundComponent';
 import FilterComponent from '../component/FilterComponent';
 import Filters from '../../domain/entities/Filters';
 
-const SoundsView = () => {
+const SoundsView = ({
+  stopAllSounds,
+  registerSound,
+}: {
+  stopAllSounds: (() => void)[];
+  registerSound: (stopSound: () => void) => void;
+}) => {
   const [sounds, setSounds] = useState([] as Sound[]);
-  const [filters, setfilters] = useState(new Filters(''));
+  const [filters, setFilters] = useState(new Filters(''));
 
   useEffect(() => setSounds(getSounds()), []);
   const onFilterUpdated = (newFilters: Filters) =>
-    setfilters(Filters.fromFilters(newFilters));
+    setFilters(Filters.fromFilters(newFilters));
+  const stopAll = () => stopAllSounds.forEach((stopSound) => stopSound());
 
   return (
     <div className="sounds-view animated fadeInRight">
-      <FilterComponent sendFiltersToParent={onFilterUpdated} />
+      <div className="actions">
+        <FilterComponent sendFiltersToParent={onFilterUpdated} />
+        <button className="button-stop-all" onClick={stopAll} type="button">
+          <FaStop className="icon-stop-all" />
+        </button>
+      </div>
+
       <div className="sounds">
         {filters.applyFilters(sounds).map((sound) => (
-          <SoundComponent key={Math.random()} sound={sound} />
+          <SoundComponent
+            key={sound.name}
+            sound={sound}
+            registerSound={registerSound}
+          />
         ))}
       </div>
     </div>
